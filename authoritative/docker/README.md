2d696f5b0299303a8215064cb475f4b7c056746e
martydingo <marty@dingo.sh>
2022-11-18 12:54:51 +0000

    use pdns official container as base image

--- authoritative/docker/README.md
+++ authoritative/docker/README.md
@@ -2,6 +2,6 @@
 
 Here lies the source for a PowerDNS Authoritative Docker image.
 
-This has been taken from the official repository found [here](https://github.com/PowerDNS/pdns), and modified to include dsc.
+The base image used been taken from the official repository found [here](https://github.com/PowerDNS/pdns), and modified to include dsc.
 
 Further information about dsc can be found [here](https://github.com/DNS-OARC/dsc)
---
+++ authoritative/docker/includes/container-entrypoint.sh
@@ -0,0 +1,26 @@
+if [[ $MMDB_ENABLED == true ]]
+then
+  if [[ $MMDB_LICENSE_KEY == "" ]]
+  then
+    echo "$MMDB_LICENSE_KEY is empty, MaxMinds Database will fail to download!"
+  else
+    export MMDB_URL="https://download.maxmind.com/app/geoip_download?license_key=${MMDB_LICENSE_KEY}&edition_id=GeoLite2"
+    cd /tmp
+    wget "$MMDB_URL-Country&suffix=tar.gz" -O MMDB-Country.tar.gz
+    wget "$MMDB_URL-ASN&suffix=tar.gz" -O MMDB-ASN.tar.gz
+
+    cp `find ./GeoLite2-ASN*/*.mmdb` /etc/dsc/ASN.mmdb
+    cp `find ./GeoLite2-Country*/*.mmdb` /etc/dsc/Country.mmdb
+
+    sed -i 's/#dataset country_code/dataset country_code/g' /etc/dsc/dsc.conf
+    sed -i 's/#dataset asn_all/dataset asn_all/g' /etc/dsc/dsc.conf
+    sed -i 's/indexer_backend geoip/indexer_backend maxminddb/g' /etc/dsc/dsc.conf
+    sed -i 's/#asn_index/asn_index/g' /etc/dsc/dsc.conf
+    sed -i 's/#country_index/country_index/g' /etc/dsc/dsc.conf
+    sed -i 's/#maxminddb_asn "\/path\/to\/GeoLite2\/ASN.mmdb"/maxminddb_asn "\/etc\/dsc\/ASN.mmdb"/g' /etc/dsc/dsc.conf
+    sed -i 's/#maxminddb_country "\/path\/to\/GeoLite2\/Country.mmdb"/maxminddb_asn "\/etc\/dsc\/Country.mmdb"/g' /etc/dsc/dsc.conf
+  fi
+fi
+
+/usr/bin/dsc /etc/dsc/dsc.conf
+/usr/bin/tini -- /usr/local/sbin/pdns_server-startup
